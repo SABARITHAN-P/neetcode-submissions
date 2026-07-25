4766412aef6b7326e98b9c8987291b93f570ae30
Sabarithan <sabarithanpalanivel@gmail.com>
2026-07-25 20:37:27 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& visited) {
+        visited[node] = 1;
+
+        for (int neigh : adj[node]) {
+            if (!visited[neigh]) {
+                if (!dfs(neigh, node, adj, visited))
+                    return false;
+            }
+            else if (neigh != parent) {
+                // Visited node which is not the parent => Cycle
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        // A tree with n nodes must have exactly n-1 edges
+        if (edges.size() != n - 1)
+            return false;
+
+        vector<vector<int>> adj(n);
+
+        for (auto &edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        vector<int> visited(n, 0);
+
+        // Start DFS from node 0
+        if (!dfs(0, -1, adj, visited))
+            return false;
+
+        // Check if all nodes were visited (graph is connected)
+        for (int i = 0; i < n; i++) {
+            if (!visited[i])
+                return false;
+        }
+
+        return true;
+    }
+};
