272dabca2aace6cf930dcd329c18a43ebc75a3ac
Sabarithan <sabarithanpalanivel@gmail.com>
2026-08-02 14:41:41 +0530

    Add: coin-change - submission-2

---
+++ Data Structures & Algorithms/coin-change/submission-2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int solve(vector<int>& coins, int amount, int ind,vector<vector<int>>& dp) {
+        if (amount == 0) return 0;
+        if (ind == coins.size()) return 1e9;
+        if (amount < 0) return 1e9;
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        int pick = 1 + solve(coins, amount - coins[ind], ind,dp);
+
+        int not_pick = solve(coins, amount, ind + 1,dp);
+
+        return dp[ind][amount]=min(pick, not_pick);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        // vector<vector<int>> dp(coins.size(),vector<int> (amount+1,-1));
+        // int ans = solve(coins, amount, 0, dp);
+
+        // if (ans >= 1e9) return -1;
+        // return ans;
+        int n = coins.size();
+        const int INF = 1e9;
+
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INF));
+
+        // Base case
+        dp[n][0] = 0;
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int amt = 0; amt <= amount; amt++) {
+
+                // Not take
+                int notPick = dp[ind + 1][amt];
+
+                // Take
+                int pick = INF;
+                if (amt >= coins[ind]) {
+                    pick = 1 + dp[ind][amt - coins[ind]];
+                }
+
+                dp[ind][amt] = min(pick, notPick);
+            }
+        }
+
+        return (dp[0][amount] >= INF) ? -1 : dp[0][amount];
+    }
+};
