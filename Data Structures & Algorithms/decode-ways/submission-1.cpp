6516dc84beaa386ff748224c1da68ef4989bc056
Sabarithan <sabarithanpalanivel@gmail.com>
2026-08-02 11:34:31 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int solve(string &s, int ind, int n, vector<int>& dp) {
+        if (ind == n)
+            return 1;
+
+        if (s[ind] == '0')
+            return 0;
+
+        if(dp[ind]!=-1) return dp[ind];
+        int pick1 = solve(s, ind + 1, n,dp);
+
+        int pick2 = 0;
+        if (ind + 1 < n &&
+            (s[ind] == '1' || (s[ind] == '2' && s[ind + 1] <= '6'))) {
+            pick2 = solve(s, ind + 2, n,dp);
+        }
+
+        return dp[ind]=pick1 + pick2;
+    }
+
+    int numDecodings(string s) {
+        
+        //return solve(s, 0, s.length(),dp);
+        int n=s.length();
+        vector<int> dp(n+1,0);
+        dp[n]=1;
+        for(int ind=n-1;ind>=0;ind--){
+            int pick1 = s[ind]!='0'?dp[ind + 1]:0;
+            int pick2 = 0;
+            if (ind + 1 < n && (s[ind] == '1' || (s[ind] == '2' && s[ind + 1] <=   '6')) and ind+1<n) {
+                pick2 = dp[ind + 2];
+            }
+            dp[ind]=pick1 + pick2;
+        }
+        return dp[0];
+    }
+};
