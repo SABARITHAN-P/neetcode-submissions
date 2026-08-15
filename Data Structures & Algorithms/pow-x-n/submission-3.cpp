0e5408febd1e92c68830cbbd087e81c327cd5cc7
Sabarithan <sabarithanpalanivel@gmail.com>
2026-08-15 10:26:54 +0530

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double solve(double x, long long n) {
+        if (n == 0)
+            return 1;
+
+        double half = solve(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+
+        return x * half * half;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0) {
+            N = -N;
+            return 1.0 / solve(x, N);
+        }
+
+        return solve(x, N);
+    }
+};
